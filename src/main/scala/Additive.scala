700fe497b87547bea5e10cbded938cea14059896
QWYNG <ikusawasi@gmail.com>
2020-04-10 00:29:44 +0900

    impl Mylang

--- src/main/scala/Additive.scala
+++ src/main/scala/Additive.scala
@@ -18,5 +18,19 @@ object Additives {
     def zero: Int = 0
   }
 
-  def sum[A](lst: List[A])(implicit m: Additive[A]): A = lst.foldLeft(m.zero)((x, y) => m.plus(x, y))
+  case class MyLang(status: String)
+
+  object MyLang {
+    // scala> sum(List(MyLang("Scala"), MyLang("Elixir")))
+    // res1: Additives.MyLang = MyLang(Ruby and Scala and Elixir)
+    implicit object MyLifeAdditive extends Additive[MyLang] {
+      def plus(a: MyLang, b: MyLang): MyLang =
+        MyLang(a.status + " and " + b.status)
+
+      def zero: MyLang = MyLang("Ruby")
+    }
+  }
+
+  def sum[A](lst: List[A])(implicit m: Additive[A]): A =
+    lst.foldLeft(m.zero)((x, y) => m.plus(x, y))
 }
